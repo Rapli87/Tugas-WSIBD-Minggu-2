494941246a31793704259573908117db190fc1d8
Rafli Al Fajar <75103755+Rapli87@users.noreply.github.com>
2021-03-16 18:44:04 +0700

    1. FIle person.java

---
+++ 1. File person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author ACER
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String fName,IName,stuId,stuStatus;
+        fName = "Rafli";
+        IName = "Al Fajar";
+        stuId = "E41200029";
+        stuStatus = "active";
+        
+        System.out.println("Student Name : " + fName + "" + IName);
+        System.out.println("Student ID : " + stuId);
+        System.out.println("Student Status : " + stuStatus); 
+    }
+    
+    
+}
